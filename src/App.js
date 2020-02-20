8a1b92d9729b4fdc378517c4497dbae176e591e1
clg <roocadon@gmail.com>
2020-02-20 16:42:44 +0100

    Add first band select option

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { firstBand } from './components/bandValues'
 
-function App() {
+import Select from "react-select";
+
+export default function App() {
+  const [firstBandOption, setFirstBandOption] = useState(null)
+
+  const handleChange = firstBandOption => {
+    setFirstBandOption(firstBandOption)
+    console.log(firstBandOption.value)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3>Resistor Calculator</h3>
       </header>
+
+      <div style={{display:'flex', justifyContent: 'space-around'}}>
+        <div style={{}}>
+          <h4>Resistor Parameters</h4>
+          <p>1st Band of Color</p>
+          <Select
+            defaultValue={firstBand[0]}
+            options={firstBand}
+            value={firstBandOption}
+            onChange={handleChange}
+            maxMenuHeight={500}
+          />
+        </div>
+        <div>
+          <h4>Output</h4>
+          <p></p>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/bandValues.js
@@ -0,0 +1,38 @@
+export const firstBand = [
+  { value: "0", label: "Black", color: "#000000" },
+  { value: "1", label: "Brown", color: "#a1674a" },
+  { value: "2", label: "Red", color: "#FF5630" },
+  { value: "3", label: "Orange", color: "#FF8B00" },
+  { value: "4", label: "Yellow", color: "#FFC400" },
+  { value: "5", label: "Green", color: "#36B37E" },
+  { value: "6", label: "Blue", color: "#3185fc" },
+  { value: "7", label: "Violet", color: "#B64DFF" },
+  { value: "8", label: "Grey", color: "#666666" },
+  { value: "9", label: "White", color: "#999999" }
+];
+
+export const multiplier = [
+  { value: "1", label: "Black", color: "#000000", isFixed: true },
+  { value: "10", label: "Brown", color: "#a1674a" },
+  { value: "100", label: "Red", color: "#FF5630" },
+  { value: "1000", label: "Orange", color: "#FF8B00" },
+  { value: "10000", label: "Yellow", color: "#FFC400" },
+  { value: "100000", label: "Green", color: "#36B37E" },
+  { value: "1000000", label: "Blue", color: "#3185fc" },
+  { value: "10000000", label: "Violet", color: "#B64DFF" },
+  { value: "100000000", label: "Grey", color: "#666666" },
+  { value: "1000000000", label: "White", color: "#999999" },
+  { value: "0.1", label: "Gold", color: "##D3AF37" },
+  { value: "0.01", label: "Silver", color: "##CCCCCC" }
+];
+
+export const tolerance = [
+  { value: "1", label: "Brown", color: "#a1674a" },
+  { value: "2", label: "Red", color: "#FF5630" },
+  { value: "0.5", label: "Green", color: "#36B37E" },
+  { value: "0.25", label: "Blue", color: "#3185fc" },
+  { value: "0.1", label: "Violet", color: "#B64DFF" },
+  { value: "0.05", label: "Grey", color: "#666666" },
+  { value: "5", label: "Gold", color: "##D3AF37" },
+  { value: "10", label: "Silver", color: "##CCCCCC" }
+];
