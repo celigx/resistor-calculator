8c443dcea915f16b1c359ce06a7b2ce3f597d80d
clg <roocadon@gmail.com>
2020-03-03 17:51:08 +0100

    Change hex color

--- src/components/bandValues.js
+++ src/components/bandValues.js
@@ -1,12 +1,12 @@
 export const firstBand = [
   { value: "0", label: "Black", color: "#000000" },
   { value: "1", label: "Brown", color: "#A1674A" },
-  { value: "2", label: "Red", color: "#FF5630" },
+  { value: "2", label: "Red", color: "#E50000" },
   { value: "3", label: "Orange", color: "#FF8B00" },
   { value: "4", label: "Yellow", color: "#FFC400" },
   { value: "5", label: "Green", color: "#36B37E" },
   { value: "6", label: "Blue", color: "#3185FC" },
-  { value: "7", label: "Violet", color: "#B64DFF" },
+  { value: "7", label: "Violet", color: "#6767CA" },
   { value: "8", label: "Grey", color: "#666666" },
   { value: "9", label: "White", color: "#999999" }
 ];
@@ -14,12 +14,12 @@ export const firstBand = [
 export const secondBand = [
   { value: "0", label: "Black", color: "#000000" },
   { value: "1", label: "Brown", color: "#A1674A" },
-  { value: "2", label: "Red", color: "#FF5630" },
+  { value: "2", label: "Red", color: "#E50000" },
   { value: "3", label: "Orange", color: "#FF8B00" },
   { value: "4", label: "Yellow", color: "#FFC400" },
   { value: "5", label: "Green", color: "#36B37E" },
   { value: "6", label: "Blue", color: "#3185FC" },
-  { value: "7", label: "Violet", color: "#B64DFF" },
+  { value: "7", label: "Violet", color: "#6767CA" },
   { value: "8", label: "Grey", color: "#666666" },
   { value: "9", label: "White", color: "#999999" }
 ];
@@ -27,12 +27,12 @@ export const secondBand = [
 export const multiplier = [
   { value: "1", label: "Black", color: "#000000" },
   { value: "10", label: "Brown", color: "#A1674A" },
-  { value: "100", label: "Red", color: "#FF5630" },
+  { value: "100", label: "Red", color: "#E50000" },
   { value: "1000", label: "Orange", color: "#FF8B00" },
   { value: "10000", label: "Yellow", color: "#FFC400" },
   { value: "100000", label: "Green", color: "#36B37E" },
   { value: "1000000", label: "Blue", color: "#3185FC" },
-  { value: "10000000", label: "Violet", color: "#B64DFF" },
+  { value: "10000000", label: "Violet", color: "#6767CA" },
   { value: "100000000", label: "Grey", color: "#666666" },
   { value: "1000000000", label: "White", color: "#999999" },
   { value: "0.1", label: "Gold", color: "#D3AF37" },
@@ -41,10 +41,10 @@ export const multiplier = [
 
 export const tolerance = [
   { value: "1", label: "Brown", color: "#A1674A" },
-  { value: "2", label: "Red", color: "#FF5630" },
+  { value: "2", label: "Red", color: "#E50000" },
   { value: "0.5", label: "Green", color: "#36B37E" },
   { value: "0.25", label: "Blue", color: "#3185fc" },
-  { value: "0.1", label: "Violet", color: "#B64DFF" },
+  { value: "0.1", label: "Violet", color: "#6767CA" },
   { value: "0.05", label: "Grey", color: "#666666" },
   { value: "5", label: "Gold", color: "#D3AF37" },
   { value: "10", label: "Silver", color: "#CCCCCC" }
